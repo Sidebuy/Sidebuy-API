a3051be9ec9a24f5f6788b7ec6f696733305dec2
Reza <reza@sidebuy.com>
2012-02-22 20:10:38 -0800

    Basic example to retrieve deals using sort and limit with JSON

--- php/example1.php
+++ php/example1.php
@@ -7,14 +7,11 @@ For any question or difficulties using this example, email reza@sidebuy.com
 $API_KEY	= 'abc62ee1fd09c296fb9f45702063a374';		// REPLACE THIS WITH YOR OWN API KEY
 $format 	= 'json';
 
-
 $query = array(
 			'city'		=>		'new-york',	// City code, for all city codes please refer to http://sidebuy.com/api/cities
 			'limit'		=>		'0-100',	// Return 100 deals. Starting from 0 to 100
 			'sort'		=>		'expiryepoch|1'	// Sort by expiry, DESC
 		);
-
-
 
 $query['format'] = $format;
 $API_END_POINT = 'http://v1.sidebuy.com/api/get/'.$API_KEY.'/?'.http_build_query($query);
@@ -29,6 +26,6 @@ $response =  json_decode($data, true);	// Decode JSON String
 //print_r($response);	// Uncomment to see all data returned
 
 foreach ($response as $deal){
-	echo "Deal title: ".htmlspecialchars_decode($deal['title'])." - Price: ".$deal['price']."<br/>";
+	echo "Deal title: ".htmlspecialchars_decode($deal['title'])." - Deal Price: ".$deal['price']."<br/>";
 }
 ?>
