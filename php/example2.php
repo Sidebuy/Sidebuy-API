76bd597dcadab2730a8ef7424642d2840c2f03ce
Reza <reza@sidebuy.com>
2012-02-23 15:44:56 -0800

    Ready to copy & paste. Automatically determines location.

--- php/example2.php
+++ php/example2.php
@@ -21,7 +21,6 @@ $query = array(
 		);
 
 $city = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR'])); // Courtesy of geoplugin.net, automatically find users latitude and longitude 
-print_r($city);
 if (isset($city)){
 	$query['loc'] = "(".$city['geoplugin_latitude'].",".$city['geoplugin_longitude'].")"; // set the latitude and longitude
 }	else {
