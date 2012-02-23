0d19dc06c607fee0735ca25ed9817b5971c1d059
Reza <reza@sidebuy.com>
2012-02-22 19:47:46 -0800

    Basic JSON with limit and sort

--- php/example1.php
+++ php/example1.php
@@ -1,5 +1,37 @@
 <?php
-// EXAMPLE1
+/*
+This example is provided by Sidebuy API Team.
+You need to change the API Key to the one you obtained from Sidebuy in Order to be able to use these examples
+For any question or difficulties using this example, email reza@sidebuy.com
+*/
+$API_KEY	= 'abc62ee1fd09c296fb9f45702063a374';		// REPLACE THIS WITH YOR OWN API KEY
+$format 	= 'json';
 
-fdd
+
+$query = array(
+			'city'		=>		'new-york',				// City code, for all city codes please refer to http://sidebuy.com/api/cities
+			'limit'		=>		'0-100',				// Return 100 deals. Starting from 0 to 100
+			'sort'		=>		'expiryepoch|1'			// Sort by expiry, DESC
+		);
+
+
+
+$query['format'] = $format;
+$API_END_POINT = 'http://v1.sidebuy.com/api/get/'.$API_KEY.'/?'.http_build_query($query);
+
+
+$ch = curl_init($API_END_POINT);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$data = curl_exec($ch); 			// Sidebuy response
+curl_close($ch);
+
+$response = json_decode($data);		// Decode JSON String
+
+
+foreach ($response as $deal){
+	echo "Deal title: ".$deal['title']."\r\n";
+}
+
+
 ?>
